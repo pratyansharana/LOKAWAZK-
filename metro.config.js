871e4c4884a88bec37b9c2bc55eb2920881eb213
pratyanshrana <pratyanshrana1@gmail.com>
2026-05-27 16:35:19 +0530

    fix: add wasm to metro asset extensions for expo-sqlite web build

--- metro.config.js
+++ metro.config.js
@@ -3,7 +3,8 @@ const { getDefaultConfig } = require('expo/metro-config');
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
-// Push 'tflite' into the existing asset extensions array
+// Push 'tflite' and 'wasm' into the existing asset extensions array
 config.resolver.assetExts.push('tflite');
+config.resolver.assetExts.push('wasm');
 
 module.exports = config;
